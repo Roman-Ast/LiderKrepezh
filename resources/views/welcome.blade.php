406ade83758c289f4f1071b5f8eb8d26b7b767fa
Roman-Ast <roman_planeta@mail.ru>
2023-12-10 09:27:50 +0500

    added animation to conacts buttons

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,6 +7,7 @@
         <link href="{{ URL::asset('css/main-big.css') }}" rel="stylesheet">
         <link href="{{ URL::asset('css/main-midi.css') }}" rel="stylesheet">
         <link href="{{ URL::asset('css/main-mini.css') }}" rel="stylesheet">
+        <link href="{{ URL::asset('css/contactsbuttons.css') }}" rel="stylesheet">
         <link rel="icon" href="images/logo-circle.png?v=2" type="image/x-icon" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
